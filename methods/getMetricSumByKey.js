e930d9d40512961b0b1ddbe3100762b34eaa75f8
Ramsing Nadeem <nadeem.ramsing@chb-technologies.ch>
2021-01-14 15:07:56 +0400

    Method._benchmark added for benchmarking

--- methods/getMetricSumByKey.js
+++ methods/getMetricSumByKey.js
@@ -3,8 +3,14 @@
 const config = require('./../app.config');
 const defaultStore = require(`./../store/${config.defaultStore}.store`);
 
-module.exports = (req, storeName = 'object') => {
-    const store = storeName ? require(`./../store/${storeName}.store`) : defaultStore;
+module.exports = main;
+
+function main(req) {
+    return { 'value': defaultStore.getSumByKey(req.params.key) };
+}
+
+main._benchmark = function (req, storeName = 'object') {
+    const store = require(`./../store/${storeName}.store`);
 
     return { 'value': store.getSumByKey(req.params.key) };
-};
+}
--- methods/postMetricByKey.js
+++ methods/postMetricByKey.js
@@ -3,10 +3,18 @@
 const config = require('./../app.config');
 const defaultStore = require(`./../store/${config.defaultStore}.store`);
 
-module.exports = (req, storeName = 'object') => {
-    const store = storeName ? require(`./../store/${storeName}.store`) : defaultStore;
+module.exports = main;
+
+function main(req) {
+    defaultStore.postValue(req.params.key, req.body.value);
+
+    return {};
+}
+
+main._benchmark = function (req, storeName = 'object') {
+    const store = require(`./../store/${storeName}.store`);
 
     store.postValue(req.params.key, req.body.value);
 
     return {};
-};
+}
